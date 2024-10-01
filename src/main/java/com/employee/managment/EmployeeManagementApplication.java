d4b2b6e5f361a433056c306d1c03df817442d236
sunnovation <poojapawandev96@gmail.com>
2024-10-01 17:41:01 +0530

    Updated Documented
    Update Main class

--- src/main/java/com/employee/managment/EmployeeManagementApplication.java
+++ src/main/java/com/employee/managment/EmployeeManagementApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeeManagementApplication {
-
+	/**
+	 * Create by Pawan Barthunia
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
